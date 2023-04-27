1d68dc6b4b3454f4fc77a291d7b1079ca8e9bb16
veljko7 <veljko_popovic_90@hotmail.com>
2023-04-27 11:36:07 +0200

    slide

--- js/build.js
+++ js/build.js
@@ -40,6 +40,38 @@ Fliplet.Widget.instance({
           thisy.data.formCount++;
           thisy.data.slideIndex++;
         });
+
+
+
+        // var formName;
+        // Fliplet.Hooks.on('beforeFormSubmit', function () {
+        //   Fliplet.FormBuilder.get()
+        //     .then(function (form) {
+        //       if (form) {
+        //         formName = form.name
+        //       }
+        //     });
+        // });
+
+        // Fliplet.Hooks.on('afterFormSubmit', function (response) {
+        //   return Fliplet.App.Storage.set(formName, response.result.id)
+        // });
+
+        // Fliplet.App.Storage.get(formName).then(function (value) {
+        //   if (value) {
+        //     Fliplet.FormBuilder.get().then(function (form) {
+        //       if (form) {
+        //         form.load(function () {
+        //           return Fliplet.DataSources.connect().then(function (connection) {
+        //             return connection.findById(value);
+        //           });
+        //         });
+        //       }
+        //     });
+        //   }
+        // });
+
+
       } else {
         thisy.data.slideIndex++;
       }
