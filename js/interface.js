68b60d4da5883cfb7dc9db91ffb000547335bdaf
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-06-20 12:00:06 +0200

    interface updated

--- js/interface.js
+++ js/interface.js
@@ -1,3 +1,5 @@
+var requiredFormDescription = '';
+
 Fliplet.Widget.generateInterface({
   title: 'slide',
   fields: [
@@ -5,10 +7,12 @@ Fliplet.Widget.generateInterface({
       name: 'requiredForm',
       type: 'radio',
       label: 'Is there a required form on this slide?',
-      description: 'Note, that selecting the screen redirection on the form settings will not work if you chose Yes',
+      description: requiredFormDescription,
       options: [{ value: true, label: 'Yes' }, { value: false, label: 'No' }],
       default: false,
       change: function(value) {
+        requiredFormDescription = value ? 'Note, that selecting the screen redirection on the form settings will not work' : '';
+        $('[data-field="requiredForm"]').find('.help-block').html(requiredFormDescription);
         Fliplet.Helper.field('requiredFormForwardNavigation').toggle(value);
         Fliplet.Helper.field('requiredFormBackwardNavigation').toggle(value);
       }
